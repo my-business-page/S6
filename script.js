c8e775a01725a81d37e95b79a9821cb86443c95b
my-business-page <madboyzzclub@gmail.com>
2026-09-25 13:31:21 +0530

    Add DoDo's Sourdough interactions

---
+++ script.js
@@ -0,0 +1,1 @@
+window.addEventListener('load',()=>document.body.classList.add('ready'));document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{e.preventDefault();document.querySelector(a.getAttribute('href'))?.scrollIntoView({behavior:'smooth'})}));
